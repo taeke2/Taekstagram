3b6642303ca7f0b60bd8bcd1ad9a49a1117e84f0
taek <taeke2@naver.com>
2023-02-24 16:23:04 +0900

    firebase email login

--- app/src/main/java/com/taek/taekstagram/login/LoginActivity.kt
+++ app/src/main/java/com/taek/taekstagram/login/LoginActivity.kt
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
 import com.taek.taekstagram.R
 import com.taek.taekstagram.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
     lateinit var binding : ActivityLoginBinding
     val loginViewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,7 @@ class LoginActivity : AppCompatActivity() {
         binding.viewModel = loginViewModel
         binding.activity = this
         binding.lifecycleOwner = this   // binding이랑 Activity랑 생명주기를 같이 할 수 있다.
+        auth = FirebaseAuth.getInstance()
         setObserver()
     }
     fun setObserver() {
@@ -32,9 +35,15 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
-    fun loginEmail() {
+    fun loginWithSignupEmail() {
         println("Email")
-        loginViewModel.showInputNumberActivity.value = true
+        auth.createUserWithEmailAndPassword(loginViewModel.id.value.toString(), loginViewModel.password.value.toString()).addOnCompleteListener { 
+            if(it.isSuccessful){
+                loginViewModel.showInputNumberActivity.value = true
+            } else {
+                // 아이디가 있을 경우
+            }
+        }
     }
 
     fun findId(){
