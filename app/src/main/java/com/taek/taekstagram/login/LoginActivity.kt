0ed622fd84dcb7ef88eb6e3ebd73d5561d529663
taek <taeke2@naver.com>
2023-02-24 16:57:18 +0900

    Google Login

--- app/src/main/java/com/taek/taekstagram/login/LoginActivity.kt
+++ app/src/main/java/com/taek/taekstagram/login/LoginActivity.kt
@@ -3,14 +3,17 @@ package com.taek.taekstagram.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.taek.taekstagram.R
 import com.taek.taekstagram.databinding.ActivityLoginBinding
 
+// MVVM 에서 화면을 통제하는 부분은 ViewModel이 가지고 있는것이 좋다.
 class LoginActivity : AppCompatActivity() {
-    lateinit var auth: FirebaseAuth
     lateinit var binding : ActivityLoginBinding
     val loginViewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +22,6 @@ class LoginActivity : AppCompatActivity() {
         binding.viewModel = loginViewModel
         binding.activity = this
         binding.lifecycleOwner = this   // binding이랑 Activity랑 생명주기를 같이 할 수 있다.
-        auth = FirebaseAuth.getInstance()
         setObserver()
     }
     fun setObserver() {
@@ -35,19 +37,20 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
-    fun loginWithSignupEmail() {
-        println("Email")
-        auth.createUserWithEmailAndPassword(loginViewModel.id.value.toString(), loginViewModel.password.value.toString()).addOnCompleteListener { 
-            if(it.isSuccessful){
-                loginViewModel.showInputNumberActivity.value = true
-            } else {
-                // 아이디가 있을 경우
-            }
-        }
-    }
+
 
     fun findId(){
         println("FindId")
         loginViewModel.showFindIdActivity.value = true
+    }
+
+    // 구글 로그인이 성공한 결과값 받는 함수
+    var googleLoginResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result ->
+
+        val data = result.data
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        val account = task.getResult(ApiException::class.java)
+        loginViewModel.firebaseAuthWithGoogle(account.idToken)  // 로그인한 사용자 정보를 암호화한 값
     }
 }
